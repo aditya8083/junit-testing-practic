29209712c912e3c349aceafd2454231e028df349
aditya.pandey <aditya.pandey0@walmartlabs.com>
2021-02-28 16:17:12 +0530

    customer controller

--- src/main/java/com/aditya/banking/system/demo/controller/CustomerController.java
+++ src/main/java/com/aditya/banking/system/demo/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.aditya.banking.system.demo.controller;
 
 import com.aditya.banking.system.demo.entity.constant.ApiPath;
+import com.aditya.banking.system.demo.entity.constant.enums.ResponseCode;
 import com.aditya.banking.system.demo.entity.dao.Customer;
 import com.aditya.banking.system.demo.model.request.CustomerModel;
 import com.aditya.banking.system.demo.service.api.CustomerService;
@@ -26,37 +27,10 @@ public class CustomerController {
     @Autowired
     RequestMappingUtils requestMappingUtils;
 
-
-    @RequestMapping(method = RequestMethod.POST)
-    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-    public ResponseEntity<Object> addCustomer(@RequestBody CustomerModel customerModel) {
-        try {
-            Customer customer = requestMappingUtils.mapCustomerModelRequest(customerModel);
-            Customer savedCustomer = customerService.saveCustomer(customer);
-            return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
-        } catch (Exception exception) {
-            LOG.error("Error in saving the customer data by User : {}, {}", customerModel.getEmail(), exception.getMessage());
-            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-    public ResponseEntity<Object> getCustomerDetails(
-                                                     @RequestParam(value = "customerId") Long customerId) {
-        try {
-            Customer savedCustomer = customerService.getCustomerDetails(customerId);
-            return new ResponseEntity<>(savedCustomer, HttpStatus.OK);
-        } catch (Exception exception) {
-            LOG.error("Error in getting the customer details : {}, {}", customerId, exception.getMessage());
-            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
     @RequestMapping(method = RequestMethod.PUT)
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<Object> updateCustomer(
-                                                 @RequestBody CustomerModel customerModel, @RequestParam(value = "customerId") Long customerId) {
+            @RequestBody CustomerModel customerModel, @RequestHeader(value = "customerId") Long customerId) {
         try {
             Customer customer = requestMappingUtils.mapCustomerModelRequest(customerModel);
             Customer updatedCustomerDetails = customerService.updateCustomer( customer, customerId);
@@ -67,13 +41,24 @@ public class CustomerController {
         }
     }
 
+    @RequestMapping(method = RequestMethod.GET)
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<Object> getCustomerDetails(@RequestHeader(value = "customerId") Long customerId) {
+        try {
+            Customer savedCustomer = customerService.getCustomerDetails(customerId);
+            return new ResponseEntity<>(savedCustomer, HttpStatus.OK);
+        } catch (Exception exception) {
+            LOG.error("Error in getting the customer details : {}, {}", customerId, exception.getMessage());
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
     @RequestMapping(method = RequestMethod.DELETE)
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Object> deleteCustomer(
-                                                 @RequestParam(value = "customerId") Long customerId) {
+    public ResponseEntity<Object> deleteCustomer(@RequestHeader(value = "customerId") Long customerId) {
         try {
             customerService.deleteCustomer(customerId);
-            return new ResponseEntity<>(null, HttpStatus.OK);
+            return new ResponseEntity<>(ResponseCode.SUCCESS.getMessage(), HttpStatus.OK);
         } catch (Exception exception) {
             LOG.error("Error in deleting the customer details : {}, {}", customerId, exception.getMessage());
             return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
--- src/main/java/com/aditya/banking/system/demo/model/request/CustomerModel.java
+++ src/main/java/com/aditya/banking/system/demo/model/request/CustomerModel.java
@@ -1,10 +1,7 @@
 package com.aditya.banking.system.demo.model.request;
 
 import com.aditya.banking.system.demo.entity.constant.enums.CustomerStatus;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Date;
 
@@ -13,20 +10,25 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerModel {
+
+    private Long id;
+    @NonNull
     private String firstName;
     private String middleName;
+    @NonNull
     private String lastName;
+    @NonNull
     private String email;
+    private String contactNo;
+    private Date dateOfBirth;
+    private String kycInfo;
+    private CustomerStatus status;
     private String address1;
     private String address2;
     private String city;
     private String state;
     private String zip;
     private String country;
-    private String contactNo;
-    private Date dateOfBirth;
-    private String kycInfo;
-    private CustomerStatus status;
 
     public String getEmail() {
         return email;
